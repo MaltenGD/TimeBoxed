6ac0067233a04802c407177ab2910475ca2b9783
Alexandra <alexlent@ucm.es>
2025-10-01 11:14:14 +0200

    animation open
    kinda rough

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function switchScreen(screenNr) {
+function openScreen() {
     const newScreen = document.getElementById("screen2");
+    const oldScreen = document.getElementById("screen1");
+
+    oldScreen.classList.add("inactive");
 
     setTimeout(() => {
         newScreen.classList.add("active");
-    }, 1000);
+    }, 500);
+}
+
+function closeScreen() {
+    const oldScreen = document.getElementById("screen1");
+    const newScreen = document.getElementById("screen2");
+
+    newScreen.classList.remove("active");
+
+    setTimeout(() => {
+        oldScreen.classList.add("active");
+    })
 }
