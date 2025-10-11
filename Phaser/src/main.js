fbdd69f76d0bed40572dcb01a7723bc9c5e4b6ff
Alexandra <alexlent@ucm.es>
2025-10-11 13:21:35 +0200

    counter button added

--- Phaser/src/main.js
+++ Phaser/src/main.js
@@ -1,4 +1,5 @@
 import { Start } from './scenes/Start.js';
+import { GameScene } from './scenes/GameScene.js';
 
 const config = {
     type: Phaser.AUTO,
@@ -10,7 +11,8 @@ const config = {
     backgroundColor: '#000000',
     pixelArt: false,
     scene: [
-        Start
+        // Start,
+        GameScene
     ],
     scale: {
         mode: Phaser.Scale.FIT,
--- Phaser/src/scenes/GameScene.js
+++ Phaser/src/scenes/GameScene.js
@@ -5,15 +5,20 @@ export class GameScene extends Phaser.Scene {
     }
 
     preload() {
-
+        this.canvas = this.sys.game.canvas;
     }
 
     create() {
-        
-    }
+        let { width, height } = this.sys.game.canvas;
 
-    update() {
-        
+        let counterTxt = 0;
+        let countText = this.add.text(width / 2, height / 2, counterTxt, { fontSize: 64 }).setOrigin(0.5)
+
+        this.counterBtn = this.add.text(width / 2, height / 2 + countText.height * 2, 'Click me!', { fontSize: 64, fill: '#0f0'}).setOrigin(0.5)
+        .setInteractive()
+        .on('pointerover', () => this.counterBtn.setStyle({fill: '#0ff'}))
+        .on('pointerdown', () => countText.setText(++counterTxt))
+        .on('pointerout', () => this.counterBtn.setStyle({fill: '#0f0'}));
     }
     
 }
