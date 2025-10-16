fb077842286ace1c5cd350741ebf2a25c8aad417
Alexandra <alexlent@ucm.es>
2025-10-16 10:36:40 +0200

    jsdocs for gameScene

--- Phaser/src/scenes/GameScene.js
+++ Phaser/src/scenes/GameScene.js
@@ -1,5 +1,10 @@
+/**
+ * Escena genérica y temporaria de juego.  
+ */
 export class GameScene extends Phaser.Scene {
-
+    /**
+     * Llama al constructor de Phaser.Scene con el nombre 'GameScene'
+     */
     constructor() {
         super('GameScene');
         this.counterTxt = 0;
@@ -10,7 +15,7 @@ export class GameScene extends Phaser.Scene {
     }
 
     create() {
-        let { width, height } = this.sys.game.canvas;
+        let { width, height } = canvas;
 
         let countText = this.add.text(width / 2, height / 2, this.counterTxt, { fontSize: 64 }).setOrigin(0.5);
 
