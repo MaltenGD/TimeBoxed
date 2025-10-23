f30e2e6e36d4d7cfdd73cc39b83a4b6711e0beb3
Alexandra <alexlent@ucm.es>
2025-10-23 10:14:47 +0200

    animation for dice

--- Phaser/src/scenes/TaliScene.js
+++ Phaser/src/scenes/TaliScene.js
@@ -93,7 +93,32 @@ export class TaliScene extends Phaser.Scene {
     rollDice() {
         this.currentRoll = this.taliGame.rollDice()
         for (let i = 0, j = -100; i < Tali.NUMBER_OF_DICE; i++, j+=100) { 
-            this.diceImages[i] = this.add.image(this.width/2 - j, this.height/2, 'dice' + this.currentRoll[i]).setOrigin(0, 0.5).setScale(0.2);
+            this.diceImages[i] = this.add.image(this.width/2 - j, this.height/2, 'dice' + this.currentRoll[i]).setOrigin(0, 0.5).setScale(0.2).setAlpha(0);
         }
+        this.animateDice();
+    }
+
+    animateDice() {
+        this.diceImages.forEach((img, index) => {
+            this.tweens.add({
+                targets: img,
+                alpha: 1,
+                duration: 1000,
+                ease: 'Sine.easeOut',
+                onComplete: () => {
+                    console.log("compelte");
+                    this.tweens.add({
+                        targets: img,
+                        alpha: 0,
+                        duration: 2000,
+                        ease: 'Sine.easeOut',
+                        onComplete: () => {
+                            console.log("compelte");
+                        }
+                    })
+                }
+            })
+        })
+        
     }
 }
