106fcdd11bcf1c4d05e0ed5e571d6c4442bdaa25
Sarahi <alicis15@ucm.es>
2025-11-02 19:58:44 +0100

    Selection Menu comments completed
    Descripcion de variables y classes, como de funcionamientos de anaimaciones y creacion de objectos

--- Phaser/src/scenes/SelectionMenuScene.js
+++ Phaser/src/scenes/SelectionMenuScene.js
@@ -1,15 +1,19 @@
+/**
+ * SelectionMenuScene class shows in the different levels of the game, so the player can choose
+ */
+
 export class SelectionMenuScene extends Phaser.Scene {
     constructor() {
         super('SelectionMenuScene');
     }
 
     create() {
-        const { width, height } = this.sys.game.canvas;
+        const { width, height } = this.sys.game.canvas;  //width and height of the canvas
         
         const centerX = width / 2;
         const centerY = height / 2;
         
-        // La caja de momento será un rectángulo simple
+        // The box, from where the 3 levels appear, for now is a plain rectangle
         const box = this.add.rectangle(centerX, centerY, 200, 200, 0xffffff)
             .setStrokeStyle(4, 0x000000)
             .setInteractive({ cursor: 'pointer' })
@@ -22,19 +26,24 @@ export class SelectionMenuScene extends Phaser.Scene {
             fontStyle: 'bold'
         }).setOrigin(0.5);
 
-        // Posiciones de los botones cuando salgan de la caja
-        const buttonGap = 380; // Espacio entre los botones
+        // Position of the buttons when they're out of the box
+        const buttonGap = 380; // Space between buttons
         const finalPositions = [
             { x: centerX - buttonGap, y: centerY-100 },  // Egypt
             { x: centerX, y: centerY -100},              // Rome
             { x: centerX + buttonGap, y: centerY -100 }   // Japan
         ];
 
+        /** array of string for the levels */
         const opciones = ['Egypt', 'Rome', 'Japan'];
+
+        /** array of string for the each level scene*/
         const scenes = ['AsebBeginScene', 'TaliBeginScene', 'HanafudaScene'];
+
+        /** Array for buttons */
         const buttons = [];
         
-        // Se crean los botones pero se dejan ocultos hasta que se haga click en la caja
+        // Levels Buttons creation but they aren't showed until the box is clicked
         opciones.forEach((opcion, index) => {
             const btn = this.add.text(centerX, centerY+150 , opcion, {
                 fontSize: '35px',
@@ -49,6 +58,7 @@ export class SelectionMenuScene extends Phaser.Scene {
             buttons.push(btn);
         });
 
+        /** Button to go back to the Start scene */
         const backBtn = this.add.text(200, height - 100, 'Volver al inicio', {
             fontSize: '30px',
             fill: '#000000',
@@ -63,15 +73,16 @@ export class SelectionMenuScene extends Phaser.Scene {
             this.scene.start('Start');
         });
 
-        // para controlar si ya salieron los botones y evitar que se repita la animación (solo se hará una vez)
+        /** It controls if the buttons are showing/deployed in screen so their animation doesn't reapeat again (it's used only 1 time)*/
         let deployed = false;
 
-        // Esto ocurre cuando se hace click en la caja
+        // When the box is clicked the follwoing happens:
+        
         box.on('pointerdown', () => {
             if (deployed) return;
             deployed = true;
 
-            // Se baja la caja abajo de la pantalla y se hace pequeña
+            // The box shrinks and goes to the bottom of the canvas
             this.tweens.add({
                 targets: [box, boxText],
                 scale: 0.5,
@@ -81,7 +92,8 @@ export class SelectionMenuScene extends Phaser.Scene {
                 ease: 'Back.easeOut'
             });
 
-            // La animacion de los botones saliendo de la caja
+            // Buttons coming our of the box animation
+
             buttons.forEach((btn, index) => {
                 this.tweens.add({
                     targets: btn,
@@ -111,7 +123,7 @@ export class SelectionMenuScene extends Phaser.Scene {
             });
         });
 
-        // Efecto hover en la caja
+        // Box hover effect
         box.on('pointerover', () => {
             if (!deployed) {
                 this.tweens.add({
