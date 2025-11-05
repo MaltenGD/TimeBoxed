1865275f636b8571e3826ef57f5515356a34d740
Alexandra <alexlent@ucm.es>
2025-11-05 17:27:01 +0100

    create transition controller

--- Phaser/src/misc/transitioncontroller.js
+++ Phaser/src/misc/transitioncontroller.js
@@ -19,10 +19,10 @@ export default class TransitionController {
      * @param {number} time - time it takes for the transition to occur. Default: 1000 ms
      * @param {RGBColor} color - color to fade to.
      */
-    startFadeOutTransition(time = 1000, color = new RGBColor(0, 0, 0)) {
+    startFadeOutTransition(time = 1000, color = new RGBColor(0, 0, 0), callback = () => {}) {
         this.camera.fadeOut(time, color.red, color.green, color.blue, (camera, progress) => {
             if (progress === 1) {
-                this.emitter.emit('fadeOutComplete');
+                callback();
             }
         });
     }
@@ -31,11 +31,12 @@ export default class TransitionController {
      * Fades in the camera.
      * @param {number} time - time it takes for the transition to occur. Default: 1000 ms
      * @param {RGBColor} color - color to fade in from. Default: (0, 0, 0) - black 
+     * @param {() => void} [callback=() => {}] The function to be called when the transition is finished.
      */
-    startFadeInTransition(time = 1000, color = new RGBColor(0, 0, 0)) {
+    startFadeInTransition(time = 1000, color = new RGBColor(0, 0, 0), callback = () => {}) {
         this.camera.fadeIn(time, color.red, color.green, color.blue, (camera, progress) => {
             if (progress === 1) {
-                this.emitter.emit('fadeInComplete');
+                callback();
             }
         });
     }
