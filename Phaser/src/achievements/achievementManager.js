dc8982dbbbbd354b2a9230c51dc61f8e674424c8
Alexandra <alexlent@ucm.es>
2025-11-15 17:04:17 +0100

    create achievement files

---
+++ Phaser/src/achievements/achievementManager.js
@@ -0,0 +1,36 @@
+import Achievement from "./achivement.js";
+
+export default class AchievementManager {
+    constructor() {
+        /** @property The total number of achievements. */
+        this.achievementNr = 0;
+
+        /** @property The map of achievements. Key: ID, value: the object */
+        this.achievementMap = new Map();
+    }
+
+    /**
+     * Loads the achievements from a json file.
+     * @param {string} jsonFile The json file to load achievements from.
+     */
+    loadAchievements(jsonFile) {
+        
+    }
+
+    /**
+     * Retrieve an achievement from the map by its key.
+     * @param {string} key The key which identifies the achievement object.
+     * @returns The achievement matching the given key.
+     */
+    getAchievementByKey(key) {
+        return map.get(key);
+    }
+
+    /**
+     * Awards the given achievement.
+     * @param {string} key The key which identifies the achievement object.
+     */
+    awardAchievement(key) {
+        map.get(key).awardAchievement();
+    }
+}
