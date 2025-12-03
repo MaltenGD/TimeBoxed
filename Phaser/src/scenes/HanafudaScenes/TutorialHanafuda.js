97ad6c50c13f89bb6a5d3e4d862d87832ed237b7
Zhiyi Zhou <zhiyiz01@ucm.es>
2025-12-03 02:01:02 +0100

    hanafuda tutorial dialogue

---
+++ Phaser/src/scenes/HanafudaScenes/TutorialHanafuda.js
@@ -0,0 +1,69 @@
+import DialogueController from "../../DialogueController.js";
+import TransitionController, {RGBColor} from "../../misc/transitioncontroller.js";
+import { BaseScene } from "../BaseScene.js";
+export class TutorialAseb extends BaseScene
+{
+    constructor()
+    {super('TutorialHanafuda');}
+
+    create(playerData)
+    {
+
+        this.playerData = playerData;
+        console.log(this.playerData) 
+
+        this.DisableOptionMenu();
+        
+         let { width, height } = this.sys.game.canvas;
+         this.width = width;
+         this.height = height;
+
+        if (this.playerData.TimeboxedMode) this.background = this.add.image(width / 2, height / 2, 'backgroundTB').setDisplaySize(width, height);
+        else this.background = this.add.image(width / 2, height / 2, 'background').setDisplaySize(width, height);
+        
+        this.transitionController = new TransitionController(this);
+        this.transitionController.startFadeInTransition();
+
+        const tutorialHanafudaData = this.cache.json.get('HanafudaTutorialdialogue');
+        this.dialogueController = new DialogueController(this, "HanafudaTutorial", tutorialHanafudaData);
+        this.dialogueController.iniDialogue();
+        
+        this.events.on('nextDialog',()=>
+        {
+            this.dialogueController.handleInteraction();
+        });
+
+        this.events.on('Finished', () => {
+
+           if (this.playerData.comingFromMenu)
+           {
+            // Stops this scene and resumes the HelpLobyScene
+            this.playerData.comingFromMenu = false;
+            this.scene.stop();
+            this.scene.resume('HelpLobbyScene', this.playerData);
+
+           }
+            else this.scene.start('HanafudaBeginScene', this.playerData)
+            
+            console.log("cambia de escena");
+        });
+
+        this.events.on('changeTutoImage',(imageKey)=> {
+            this.changeTutoImage(imageKey);
+        });
+    }
+
+    changeTutoImage(imageKey)
+    {
+        if(this.tutoImage)
+        {
+            this.tutoImage.destroy();
+        }
+
+        if (imageKey != "none") this.tutoImage = this.add.image(this.width/2, this.height/2, imageKey).setOrigin(0.5).setScale(1);
+    }
+
+
+
+    
+}
