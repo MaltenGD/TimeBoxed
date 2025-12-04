da01bf1c255407883fa98657a713afd78cd19dc5
Alexandra <alexlent@ucm.es>
2025-12-04 21:08:56 +0100

    add hanafuda insignia

--- Phaser/src/scenes/LoadingScene.js
+++ Phaser/src/scenes/LoadingScene.js
@@ -248,7 +248,7 @@ export class LoadingScene extends Phaser.Scene
     }
 
     loadInisgniaAssets() {
-        this.load.image('tempInsignia3', 'Phaser/assets/insignias/tempInsignia3.png');
+        this.load.image('Card_God', 'Phaser/assets/insignias/Card_God.png');
         this.load.image('Underworld_Conqueror', 'Phaser/assets/insignias/Underworld_Conqueror.png');
         this.load.image('Square_Master', 'Phaser/assets/insignias/Square_Master.png');
         this.load.image('Wonder_of_Egypt', 'Phaser/assets/insignias/Wonder_of_Egypt.png');
