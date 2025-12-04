2b64e73aff1a4f8d408633308d8f3b88a8d2cfa6
Alexandra <alexlent@ucm.es>
2025-12-04 19:45:31 +0100

    Throws shop up nicely arranged now.

--- Phaser/src/tali/tali.js
+++ Phaser/src/tali/tali.js
@@ -83,8 +83,8 @@ export default class Tali {
      * Adds all the images.
      */
     addImages() {
-        for (let i = 0, j = 1; i < Tali.DICE_THROW_NAMES.length; i++, j++) {
-            this.throwImages[i] = this.scene.add.image(j*this.width/5, this.height/2, Tali.DICE_THROW_NAMES[i]).setOrigin(0.5).setAlpha(0).setScale(0.4);
+        for (let i = 0, j = -600; i < Tali.DICE_THROW_NAMES.length; i++, j+=300) {
+            this.throwImages[i] = this.scene.add.image(this.width/2 + j, this.height/2, Tali.DICE_THROW_NAMES[i]).setOrigin(0.5).setAlpha(0).setScale(0.35);
             this.throwImages[i].depth = 1;
         }
     }
