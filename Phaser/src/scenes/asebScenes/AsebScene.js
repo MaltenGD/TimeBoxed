f59fc505d8ed59f8644e95c09903876dfe643059
Oliver García <oliver.garcia.aguado@gmail.com>
2025-12-12 00:54:47 +0100

    debugMode disabler removed
    WHO DID THIS

--- Phaser/src/scenes/asebScenes/AsebScene.js
+++ Phaser/src/scenes/asebScenes/AsebScene.js
@@ -39,7 +39,6 @@ export class AsebScene extends BaseScene {
 
         this.playerData = playerData;
         console.log(this.playerData)
-        this.playerData.DebugMode = false;
 
         // Default to player going first if no data is passed.
             if (playerData !== undefined) this.playerFirst = playerData.AsebPlayerFirst
